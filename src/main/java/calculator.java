a9e11f75d713e9740da88abe1375eddfadddcb67
Shriya Kabra <shriyakabra97@gmail.com>
2020-04-08 16:36:11 +0530

    added Add feature and dependencies

---
+++ src/main/java/calculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class calculator{
+    public static void main(String args[]){
+        float a, b, res ;
+        char choice, ch ;
+        Scanner sc = new Scanner(System.in);
+
+        do{
+            System.out.println("1. ADD TWO Numbers");
+            System.out.println("2. EXIT");
+            System.out.println("Enter your choice");
+            choice = sc.next().charAt(0);
+            switch(choice){
+                case '1' :
+                    System.out.println("Enter two numbers :");
+                    a = sc.nextFloat();
+                    b = sc.nextFloat();
+                    res = a+b ;
+                    System.out.println("Result = " + res);
+                    break;
+                case '2' :
+                    System.exit(0);
+                    break;
+
+                default :
+                    System.out.println("INVALID CHOICE!!");
+                    break;
+
+            }
+            System.out.println("----------------------------------------");
+        }while(choice!=2);
+    }
+
+}
